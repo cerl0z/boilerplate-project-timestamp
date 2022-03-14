b1929de3d62e09594bda3b04e6cd43a0e2b87e7c
Carlos Peña <cpena3@u.rochester.edu>
2022-03-14 15:50:04 -0400

    submit project for testing

--- server.js
+++ server.js
@@ -19,11 +19,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// your first API endpoint...
-// app.get("/api/hello", function (req, res) {
-//   res.json({ greeting: "hello API" });
-// });
-
 app.get("/api/:date?", (req, res) => {
   let dateParams = req.params.date;
   const date = new Date(dateParams);
